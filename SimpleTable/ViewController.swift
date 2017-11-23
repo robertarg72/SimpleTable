470b89d2526e9b81bdd1fbb4652f76ebf8f62862
Robert Argume <robert@roberts-macbook-pro.local>
2017-11-22 19:48:53 -0500

    Add table view and basic content

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let movies = [
+        "Batman vs Superman", "Blade Runner 2049", "Thor: Ragnorok", "Justice League", "Wonderwoman", "Dark Tower", "Star Wars: The Last Jedi"
+    ]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: simpleTableIdentifier)
+        }
+        cell?.textLabel?.text = movies[indexPath.row]
+        return cell!
+    }
 
 
 }
